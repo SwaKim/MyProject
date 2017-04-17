c99e1a1bb29badcd2656b57ae6523d3caa885856
DESKTOP-C0N9JKG\kkk32 <kkk3206@naver.com>
2017-04-17 10:53:43 +0900

    no message

---
+++ src/main/java/com/Bluswa/tistory/BlueSwaApplication.java
@@ -0,0 +1,12 @@
+package com.Bluswa.tistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSwaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Bluswa/tistory/BlueSwaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Bluswa.tistory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlueSwaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
